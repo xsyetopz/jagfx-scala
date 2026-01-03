9ad6a23fb6e8c34f2cf4ec4a4fc031fa679904b8
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2026-01-03 06:42:12 +0200

    envelope fix for flters

--- src/main/scala/jagfx/synth/EnvelopeEvaluator.scala
+++ src/main/scala/jagfx/synth/EnvelopeEvaluator.scala
@@ -21,28 +21,33 @@ class EnvelopeEvaluator(envelope: Envelope):
     threshold = 0
     position = 0
     delta = 0
-    amplitude = 0
+    amplitude = envelope.start << 15
     ticks = 0
 
   /** Evaluates envelope at current tick, advancing internal state. Returns
     * interpolated value scaled to `0`-`65535` range.
     */
-  def evaluate(deltaArg: Int): Int =
+  def evaluate(period: Int): Int =
     if envelope.segments.isEmpty then return envelope.start
 
     if ticks >= threshold then
       amplitude = envelope.segments(position).peak << 15
       position += 1
+
       if position >= envelope.segments.length then
         position = envelope.segments.length - 1
+
       threshold = ((envelope
         .segments(position)
         .duration
-        .toDouble / 65536.0) * deltaArg).toInt
+        .toDouble / 65536.0) * period).toInt
+
       if threshold > ticks then
         delta = ((envelope
           .segments(position)
           .peak << 15) - amplitude) / (threshold - ticks)
+      else delta = 0
+
     amplitude += delta
     ticks += 1
     (amplitude - delta) >> 15
