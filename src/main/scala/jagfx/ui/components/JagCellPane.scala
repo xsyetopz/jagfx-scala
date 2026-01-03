8f1e9fa86039544779cae2594246fb1e00ebf371
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2026-01-03 02:32:23 +0200

    remove Solo/Mute as this synth doesn't need them on each cell (only on tones if ever)

--- src/main/scala/jagfx/ui/components/JagCellPane.scala
+++ src/main/scala/jagfx/ui/components/JagCellPane.scala
@@ -3,21 +3,17 @@ package jagfx.ui.components
 import javafx.beans.property._
 import javafx.scene.layout._
 import javafx.scene.control.Label
-import javafx.scene.control.{ContextMenu, MenuItem, SeparatorMenuItem}
+import javafx.scene.control._
 import javafx.geometry.Pos
 import jagfx.ui.viewmodel.EnvelopeViewModel
 import jagfx.utils.IconUtils
 
 class JagCellPane(title: String) extends StackPane:
   private val collapsed = SimpleBooleanProperty(false)
-  private val muted = SimpleBooleanProperty(false)
   private val selected = SimpleBooleanProperty(false)
-  private val solo = SimpleBooleanProperty(false)
 
   def collapsedProperty: BooleanProperty = collapsed
-  def mutedProperty: BooleanProperty = muted
   def selectedProperty: BooleanProperty = selected
-  def soloProperty: BooleanProperty = solo
 
   getStyleClass.add("jag-cell")
   setMinWidth(0)
@@ -42,11 +38,6 @@ class JagCellPane(title: String) extends StackPane:
   private val btnX1 = createToolButton("X1")
   private val btnX2 = createToolButton("X2")
   private val btnX4 = createToolButton("X4")
-  private val btnS = createToolButton("")
-  btnS.setGraphic(IconUtils.icon("mdi2s-star"))
-
-  private val btnM = createToolButton("")
-  btnM.setGraphic(IconUtils.icon("mdi2v-volume-off"))
 
   private val btnCollapse = createToolButton("")
   btnCollapse.setGraphic(IconUtils.icon("mdi2c-chevron-up"))
@@ -54,25 +45,24 @@ class JagCellPane(title: String) extends StackPane:
   private val btnMenu = createToolButton("")
   btnMenu.setGraphic(IconUtils.icon("mdi2d-dots-horizontal"))
 
-  private val zooms = Seq(btnX1, btnX2, btnX4)
-  zooms.foreach(b =>
-    b.setOnAction(_ =>
-      zooms.foreach(_.setActive(false))
-      b.setActive(true)
+  private val zooms = Seq((btnX1, 1), (btnX2, 2), (btnX4, 4))
+  private var alternateCanvas: Option[JagBaseCanvas] = None
+
+  /** Set alternate canvas that zoom buttons will control instead of built-in
+    * envelope canvas.
+    */
+  def setAlternateCanvas(alt: JagBaseCanvas): Unit =
+    alternateCanvas = Some(alt)
+    btnX1.fire() // force [X1] zoom
+
+  zooms.foreach { case (btn, level) =>
+    btn.setOnAction(_ =>
+      zooms.foreach(_._1.setActive(false))
+      btn.setActive(true)
+      alternateCanvas.getOrElse(canvas).setZoom(level)
     )
-  )
+  }
   btnX1.setActive(true)
-
-  btnS.setOnAction(_ =>
-    val s = !solo.get
-    solo.set(s)
-    if s then btnS.setActive(true) else btnS.setActive(false)
-  )
-
-  btnM.setOnAction(_ =>
-    muted.set(!muted.get)
-    btnM.setActive(muted.get)
-  )
 
   private val contextMenu = new ContextMenu()
 
@@ -81,12 +71,7 @@ class JagCellPane(title: String) extends StackPane:
     val iX1 = new MenuItem("x1"); iX1.setOnAction(_ => btnX1.fire())
     val iX2 = new MenuItem("x2"); iX2.setOnAction(_ => btnX2.fire())
     val iX4 = new MenuItem("x4"); iX4.setOnAction(_ => btnX4.fire())
-    val iS = new MenuItem(if solo.get then "Un-Solo" else "Solo");
-    iS.setOnAction(_ => btnS.fire())
-    val iM = new MenuItem(if muted.get then "Un-Mute" else "Mute");
-    iM.setOnAction(_ => btnM.fire())
-
-    contextMenu.getItems.addAll(iX1, iX2, iX4, new SeparatorMenuItem(), iS, iM)
+    contextMenu.getItems.addAll(iX1, iX2, iX4)
 
   btnMenu.setOnAction(e =>
     updateMenu()
@@ -95,11 +80,9 @@ class JagCellPane(title: String) extends StackPane:
 
   btnCollapse.setOnAction(_ => collapsed.set(!collapsed.get))
 
-  private var showMute = true
   private var showCollapse = true
 
   def setFeatures(showMute: Boolean, showCollapse: Boolean): Unit =
-    this.showMute = showMute
     this.showCollapse = showCollapse
     updateToolbar()
 
@@ -108,8 +91,7 @@ class JagCellPane(title: String) extends StackPane:
     val w = getWidth
 
     val titleWidth = titleLabel.prefWidth(-1)
-    var toolsCount = 4 // X1, X2, X4, S
-    if showMute then toolsCount += 1
+    var toolsCount = 3 // X1, X2, X4
     if showCollapse then toolsCount += 1
 
     val toolsWidth = toolsCount * 25
@@ -120,8 +102,7 @@ class JagCellPane(title: String) extends StackPane:
       toolbar.getChildren.add(btnMenu)
       if showCollapse then toolbar.getChildren.add(btnCollapse)
     else
-      toolbar.getChildren.addAll(btnX1, btnX2, btnX4, btnS)
-      if showMute then toolbar.getChildren.add(btnM)
+      toolbar.getChildren.addAll(btnX1, btnX2, btnX4)
       if showCollapse then toolbar.getChildren.add(btnCollapse)
 
   widthProperty.addListener((_, _, _) => updateToolbar())
@@ -148,11 +129,6 @@ class JagCellPane(title: String) extends StackPane:
       getStyleClass.remove("collapsed")
       btnCollapse.setGraphic(IconUtils.icon("mdi2c-chevron-up"))
       canvas.setVisible(true)
-  )
-
-  muted.addListener((_, _, isMuted) =>
-    if isMuted then getStyleClass.add("muted")
-    else getStyleClass.remove("muted")
   )
 
   selected.addListener((_, _, isSelected) =>
